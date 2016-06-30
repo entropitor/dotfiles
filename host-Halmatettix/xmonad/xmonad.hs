cc547e068f7f7953d1bc875ff23945a013f0de7a
Jens Claes <jensclaes33@gmail.com>
2016-06-30 14:10:15 +0200

    Volume fixed

--- host-Halmatettix/xmonad/xmonad.hs
+++ host-Halmatettix/xmonad/xmonad.hs
@@ -1,8 +1,10 @@
 import XMonad
+import XMonad.Actions.Volume
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Util.Dzen
 import System.IO
 
 main = do
@@ -16,14 +18,24 @@ main = do
             , ppTitle = xmobarColor "green" "" . shorten 50
             }
     , modMask = mod4Mask   -- Rebind Mod to the Windows key
+    , terminal = "gnome-terminal"
     } `additionalKeys`
-    [
-      ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s"),
-      ((0, xK_Print), spawn "scrot")
-      ,
+    [ ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+      , ((0, xK_Print), spawn "scrot")
       -- http://superuser.com/questions/389737/how-do-you-make-volume-keys-and-mute-key-work-in-xmonad
-      ((0, 0x1008FF11), spawn "amixer set Master 2-"),
-      ((0, 0x1008FF13), spawn "amixer set Master 2+")
-      -- ,
-      -- ((0, 0x1008FF12), spawn "amixer set Master toggle")
+      -- http://dmwit.com/volume/
+      , ((0, 0x1008FF11), lowerVolume 5 >>= alertDouble)
+      , ((0, 0x1008FF13), raiseVolume 5 >>= alertDouble)
+      , ((0, 0x1008FF12), toggleMuteChannels ["Master", "Speaker", "Headphone", "Headphone 1"] >>= \muted -> alert (if muted then "off" else "on"))
     ]
+
+alertDouble = alert . show . bound . round
+  where bound x | x < 0     = 0
+                | x > 100   = 100
+                | otherwise = x
+alert = dzenConfig centered
+  where centered = onCurr (center 150 66)
+                    >=> font "-*-helvetica-*-r-*-*-64-*-*-*-*-*-*-*"
+                    >=> addArgs ["-fg", "#ffffff"]
+                    >=> addArgs ["-bg", "#000040"]
+

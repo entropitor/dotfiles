5a89196a59dab4715c48f7a48129a5beb0cda896
Jens Claes <jensclaes33@gmail.com>
2016-07-05 23:07:39 +0200

    Workspaces and layouts

--- host-Halmatettix/xmonad/xmonad.hs
+++ host-Halmatettix/xmonad/xmonad.hs
@@ -1,19 +1,91 @@
 import XMonad
 import XMonad.Actions.Volume
+import XMonad.Layout.PerWorkspace (onWorkspace)
+import XMonad.Layout.NoBorders
+import XMonad.Layout.ShowWName
+import XMonad.Layout.Tabbed
+import XMonad.Layout.Grid
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.SetWMName
+import XMonad.Hooks.UrgencyHook
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import XMonad.Util.Dzen
 import System.IO
 
+-- general config
 altMask = mod1Mask
 myModMask = mod4Mask
+myTerminal = "gnome-terminal"
+
+
+-- My workspaces
+myWorkspace1 = "1:WF"
+myWorkspace2 = "2:Pinned"
+myWorkspace3 = "3:Chrome"
+myWorkspace4 = "4:Term"
+myWorkspace5 = "5:Main"
+myWorkspace6 = "6:Extra"
+myWorkspace7 = "7:Mail"
+myWorkspace8 = "8:Hang"
+myWorkspace9 = "9:Slack"
+myWorkspaces =
+  [
+    myWorkspace1, myWorkspace2, myWorkspace3,
+    myWorkspace4, myWorkspace5, myWorkspace6,
+    myWorkspace7, myWorkspace8, myWorkspace9
+  ]
+startupWorkspace = myWorkspace5
+
+-- My Layouts
+defaultLayouts = showWName (tiled ||| simpleTabbedBottom ||| Grid ||| noBorders Full ||| Mirror tiled)
+  where tiled = Tall 1 (3/100) (1/2)
+--defaultLayouts = Tall 1 (3/100) (1/2) ||| Full
+--myLayouts = onWorkspace myWorkspace1 Full $ defaultLayouts
+myLayouts = defaultLayouts
+
+-- My key bindings
+myKeyBindings =
+  [ ((0, xK_Print), spawn "sleep 0.2; gnome-screenshot -c -a") -- sleep to get keyboard!
+    , ((controlMask, xK_Print), spawn "gnome-screenshot -c -w")
+    , ((controlMask .|. shiftMask, xK_Print), spawn "gnome-screenshot -i -a")
+    -- http://superuser.com/questions/389737/how-do-you-make-volume-keys-and-mute-key-work-in-xmonad
+    -- http://dmwit.com/volume/
+    , ((0, 0x1008FF11), lowerVolume 5 >>= alertDouble)
+    , ((0, 0x1008FF13), raiseVolume 5 >>= alertDouble)
+    , ((0, 0x1008FF12), toggleMuteChannels ["Master", "Speaker", "Headphone", "Headphone 1"] >>= \muted -> alert (if muted then "off" else "on"))
+    -- old terminal shortcut
+    , ((controlMask .|. altMask, xK_t), spawn myTerminal)
+    , ((myModMask, xK_e), spawn "nautilus --new-window")
+  ]
 
 {-
-  Alerts for sound settings
+  Main configuration
 -}
+main = do
+  xmproc <- spawnPipe "xmobar ~/.xmonad/xmobarrc"
+  xmonad $ withUrgencyHook NoUrgencyHook $ defaultConfig {
+    manageHook = manageDocks <+> manageHook defaultConfig
+    , logHook = dynamicLogWithPP xmobarPP {
+        ppOutput = hPutStrLn xmproc
+        , ppTitle = xmobarColor "#abc" "" . shorten 70
+        , ppCurrent = xmobarColor "#e6744c" "" . wrap "[" "]"
+        , ppVisible = xmobarColor "#c185a7" "" . wrap "(" ")"
+        , ppUrgent = xmobarColor "#c00" "" . wrap "{" "}"
+        , ppHidden = xmobarColor "#ccc" ""
+        , ppHiddenNoWindows = xmobarColor "#333" ""
+      }
+    , modMask = myModMask
+    , terminal = myTerminal
+    , workspaces = myWorkspaces
+    , layoutHook = avoidStruts $ myLayouts
+    , startupHook = do
+        setWMName "LG3D"
+        spawn "~/.xmonad/startup-hook"
+  } `additionalKeys` myKeyBindings
+
+-- Alerts for sound settings
 alertDouble = alert . show . bound . round
   where bound x | x < 0     = 0
                 | x > 100   = 100
@@ -23,42 +95,3 @@ alert = dzenConfig centered
                     >=> font "-*-helvetica-*-r-*-*-64-*-*-*-*-*-*-*"
                     >=> addArgs ["-fg", "#ffffff"]
                     >=> addArgs ["-bg", "#000040"]
-
-
-{-
-  Main configuration
--}
-main = do
-  xmproc <- spawnPipe "xmobar ~/.xmonad/xmobarrc"
-  let config = defaultConfig {
-      manageHook = manageDocks <+> manageHook defaultConfig
-      , layoutHook = avoidStruts  $  layoutHook defaultConfig
-      , logHook = dynamicLogWithPP xmobarPP
-        { ppOutput = hPutStrLn xmproc
-          , ppTitle = xmobarColor "#abc" "" . shorten 80
-          , ppCurrent = xmobarColor "#e6744c" "" . wrap "[" "]"
-          , ppVisible = xmobarColor "#c185a7" "" . wrap "(" ")"
-          , ppUrgent = xmobarColor "#c00" "" . wrap "{" "}"
-          , ppHidden = xmobarColor "#ccc" ""
-          , ppHiddenNoWindows = xmobarColor "#333" ""
-        }
-      , modMask = myModMask   -- Rebind Mod to the Windows key
-      , terminal = "gnome-terminal"
-      , startupHook = do
-          setWMName "LG3D"
-          spawn "~/.xmonad/startup-hook"
-    }
-
-  xmonad $ config `additionalKeys`
-    [ ((0, xK_Print), spawn "sleep 0.2; gnome-screenshot -c -a") -- sleep to get keyboard!
-      , ((controlMask, xK_Print), spawn "gnome-screenshot -c -w")
-      , ((controlMask .|. shiftMask, xK_Print), spawn "gnome-screenshot -i -a")
-      -- http://superuser.com/questions/389737/how-do-you-make-volume-keys-and-mute-key-work-in-xmonad
-      -- http://dmwit.com/volume/
-      , ((0, 0x1008FF11), lowerVolume 5 >>= alertDouble)
-      , ((0, 0x1008FF13), raiseVolume 5 >>= alertDouble)
-      , ((0, 0x1008FF12), toggleMuteChannels ["Master", "Speaker", "Headphone", "Headphone 1"] >>= \muted -> alert (if muted then "off" else "on"))
-      -- old terminal shortcut
-      , ((controlMask .|. altMask, xK_t), spawn $ terminal config)
-      , ((myModMask, xK_e), spawn "nautilus --new-window")
-    ]

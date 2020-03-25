75556039702f07e7a85ab3d24f94972eb621b071
Jens Claes <jensclaes33@gmail.com>
2020-03-25 12:16:48 +0100

    (zabai): Add script to fix padding for large monitor

--- host-Dinotettix.local/bin/zabai.sh
+++ host-Dinotettix.local/bin/zabai.sh
@@ -23,4 +23,31 @@ function reload() {
   osascript -e 'tell application id "tracesOf.Uebersicht" to refresh widget id "yabar-tertiary-display-jsx"'
 }
 
+function mid_for_label() {
+  local label="@:$1"
+
+  yabai -m query --spaces | jq ".[] | select(.label==\"${label}\").index"
+}
+
+function set_padding_label() {
+  local label_index=$1
+
+  set_padding_mid $(mid_for_label $label_index)
+}
+
+function set_padding_mid() {
+  local index="$1"
+  local amount="${2:-130}"
+  local where="${3:-"bottom"}"
+
+  yabai -m config --space ${index} ${where}_padding ${amount}
+}
+
+function fix_padding() {
+  local amount="${1:-130}"
+
+  yabai -m query --displays | jq '.[] | select(.frame.w==2560.0000 and .frame.h ==1440.000).spaces[]' | xargs -n1 -I% $0 set_padding_mid % ${amount} bottom
+  yabai -m query --displays | jq '.[] | select((.frame.w==2560.0000 and .frame.h ==1440.000) | not).spaces[]' | xargs -n1 -I% $0 set_padding_mid % 0 bottom
+}
+
 "$@"

95b68ef6c96d90265d628a6dd4acb833fc6d4f45
Jens Claes <jens@datacamp.com>
2021-02-12 23:58:17 +0100

    (vim): Use custom code action to get code-actions for all connected clients

---
+++ tag-vim/config/nvim/lua/entropitor/hello.lua
@@ -0,0 +1,61 @@
+local M = {}
+
+function M.__reload ()
+  package.loaded['entropitor.hello'] = nil
+  print('Reloaded entropitor.hello')
+end
+
+function M.code_action ()
+  local params = vim.lsp.util.make_range_params()
+  params.context = {
+    diagnostics = vim.lsp.diagnostic.get_line_diagnostics()
+  }
+
+  local received = {}
+  local requested = { ids = {} }
+  local handler = function (_, _, received_actions, received_client_id, bufnr)
+    if received_client_id == nil then
+      return
+    end
+    received[received_client_id]=received_actions
+
+    local received_all = true
+    for client_id, request_id in pairs(requested.ids) do
+      if request_id == nil then
+        goto continue
+      end
+
+      if received[client_id] == nil then
+        received_all = false
+        break
+      end
+
+      ::continue::
+    end
+
+    if not received_all then
+      return
+    end
+
+    local all = {}
+    for _, actions in pairs(received) do
+      if actions ~= nil then
+        for _, action in ipairs(actions) do
+          table.insert(all, action)
+        end
+      end
+    end
+    print(vim.inspect(all))
+    vim.lsp.handlers['textDocument/codeAction'](nil, 'textDocument/codeAction', all, 0, bufnr)
+  end
+
+  requested.ids = vim.lsp.buf_request(0, 'textDocument/codeAction', params, handler)
+end
+
+function M.all_code_actions ()
+  vim.lsp.buf.code_action({
+    diagnostics = vim.lsp.diagnostic.get()
+  })
+end
+
+return M

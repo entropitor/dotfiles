53b8500669dda9b72f82d3a007807cc134785bb5
Jens Claes <jens@datacamp.com>
2021-06-10 17:48:21 +0200

    (vim): Remove automatically highlight word under cursor

--- tag-vim/config/nvim/lua/entropitor/lsp/servers.lua
+++ tag-vim/config/nvim/lua/entropitor/lsp/servers.lua
@@ -24,7 +24,6 @@ local on_attach = function(client, _bufnr)
       hi LspReferenceWrite gui=undercurl guisp=White
       augroup lsp_document_highlight
         autocmd! * <buffer>
-        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
         autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
       augroup END
     ]],
